e01ab9b78e5362922d64a6b11f0ba4275a30ad49
re renn <neechan354@gmail.com>
2023-12-16 15:15:48 +0700

    Rangguna
    Menambah data zoo

--- lib/data/zoo_data.dart
+++ lib/data/zoo_data.dart
@@ -17,15 +17,15 @@ var zooList = [
     isFavorite: false,
   ),
   Zoo(
-    nama: 'Taman Wisata Safari Zoo',
-    lokasi: 'Bogor, Jawa Barat',
+    nama: 'Taman Margasatwa Ragunan',
+    lokasi: 'Jakarta, Indonesia',
     deskripsi:
-    'Taman Wisata Kebun Binatang Safari Indonesia adalah salah satu destinasi wisata unggulan di Bogor, Jawa Barat. Dengan koleksi hewan yang beragam dan atraksi menarik, tempat ini menawarkan pengalaman menyenangkan untuk semua usia. Dapatkan kesempatan untuk melihat hewan-hewan liar dari dekat dan nikmati berbagai kegiatan seru di taman ini.',
-    dibangun: 'Tahun 1986',
-    luas: '170 hektar',
-    imageAsset: 'images/Safari.jpeg',
+    'Taman Margasatwa Ragunan adalah sebuah kebun binatang yang terletak di Jakarta, Indonesia. Kebun binatang ini memiliki luas sekitar 140 hektar dan menjadi rumah bagi beragam spesies hewan, termasuk satwa langka dan dilindungi. Taman Margasatwa Ragunan juga menyediakan berbagai fasilitas dan kegiatan edukasi untuk pengunjung.',
+    dibangun: 'Tahun 1864',
+    luas: '140 hektar',
+    imageAsset: 'images/Ragunan.jpeg',
     imageUrls: [
-      'https://img.harianjogja.com/posts/2023/12/10/1157755/wisata_puncak_-_taman_safari_indonesia_1671529719.jpg',
+      'https://statik.tempo.co/data/2021/01/03/id_991679/991679_720.jpg',
 
     ],
     isFavorite: false,
