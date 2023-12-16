49d51b5b9dc256df17109ea1d8149f899e2a9b1e
re renn <neechan354@gmail.com>
2023-12-16 14:54:24 +0700

    Initial commit

---
+++ lib/data/zoo_data.dart
@@ -0,0 +1,165 @@
+
+import 'package:wisata_zoopedia/models/zoo.dart';
+
+var zooList = [
+  Zoo(
+    nama: 'Candi Borobudur',
+    lokasi: 'Magelang, Jawa Tengah',
+    deskripsi:
+        'Candi Borobudur, candi Buddha terbesar di dunia, dibangun oleh penganut Buddha Mahayana pada abad ke-9 di Jawa Tengah, Indonesia. Dengan enam teras dan tiga pelataran melingkar yang dihiasi relief dan arca Buddha, Borobudur adalah tempat suci untuk memuliakan Buddha dan panduan ziarah menuju pencerahan. Meskipun ditinggalkan pada abad ke-10, candi ini ditemukan kembali pada tahun 1814 dan setelah pemugaran besar-besaran, diakui sebagai Situs Warisan Dunia oleh UNESCO.',
+    dibangun: 'Abad ke-9',
+    luas: 'Candi Buddha',
+    imageAsset: 'images/borobudur.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/11/cd/4f/71/borobudur-and-the-sky.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/11/cd/5d/36/borobudur-airial-view.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/11/cd/4d/c7/borobudur-and-the-sky.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/11/cd/47/0e/borobudur-sunset-moment.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+        'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+  Zoo(
+    nama: '',
+    lokasi: '',
+    deskripsi:
+    'Candi Mendut adalah candi Buddha yang terletak di Desa Mendut, Kabupaten Magelang, Jawa Tengah, sekitar 3 kilometer dari Candi Borobudur. Dibangun pada masa pemerintahan Raja Indra dari dinasti Syailendra, Candi Mendut dikaitkan dengan prasasti Karangtengah tahun 824 Masehi, yang mengaitkannya dengan istilah "wenuwana" atau hutan bambu. Candi ini dibangun dengan batu bata dan lapisan batu alam, berdiri teguh di atas basement tinggi dengan pintu masuk menghadap barat-daya. Bangunan ini memiliki lorong mengelilingi tubuhnya, atap tiga tingkat, dan dihiasi dengan 48 stupa kecil di bagian puncaknya.',
+    dibangun: '',
+    luas: '',
+    imageAsset: 'images/mendut.jpeg',
+    imageUrls: [
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/8d/bd/dc/photo1jpg.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
+      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
+    ],
+    isFavorite: false,
+  ),
+];
